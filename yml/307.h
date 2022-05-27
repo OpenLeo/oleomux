36b6a4e2dc8b0f3411efab5ae2933e90f434a7e9
lorddevereux <lorddevereux@outlook.com>
2022-05-27 22:35:08 +0100

    Removed unneeded files

--- yml/307.h
+++
@@ -1,5 +0,0 @@
-#include "307_messages.h"
-
-void ole07_parse_commandes_bsi(can_msg* msg, ole07_commandes_bsi* ptr);
-void ole07_parse_donnees_bsi_rapides(can_msg* msg, ole07_donnees_bsi_rapides* ptr);
-void ole07_parse_donnees_bsi_lentes(can_msg* msg, ole07_donnees_bsi_lentes* ptr);
